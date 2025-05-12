883db45baa24d28330691165f3e9da091182f7aa
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-05-12 23:15:55 +0200

    added: 12 may 23:15

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# js-practice-codewars
+# coding-practice
--- fundamentals/codewars/list1.js
+++
@@ -1,8 +0,0 @@
-// 1
-// Your task is to remove all duplicate words from a
-// string, leaving only single (first) words entries.
-function removeDuplicateWords(s) {
-  let stringArr = s.split(" ");
-  let stringSet = new Set(stringArr);
-  return [...stringSet].join(" ");
-}
